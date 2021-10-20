a4fad30b203e40f0c4c663b1f167938d832f38e9
Samet Kamgul <sametkamgul>
2021-10-20 20:29:26 +0300

    express was forgotten to added to the package.json.

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const serveStatic = require("serve-static")
 const path = require('path');
 app = express();
-app.use(serveStatic(path.join(__dirname, 'src')));
+app.use(serveStatic(path.join(__dirname, 'dist')));
 const port = process.env.PORT || 3000;
-app.listen(port);
+app.listen(port, () => {
+    console.log(`server running on port: ${port}`);
+});
