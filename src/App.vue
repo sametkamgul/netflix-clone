41df9088b96fa9b199ce4be524dfd8f7a052bdd2
Samet Kamgul <sametkamgul>
2021-10-23 21:20:27 +0300

    some changes for responsive

--- src/App.vue
+++ src/App.vue
@@ -50,13 +50,16 @@ export default {
     methods: {
         async getMovies() {
             console.log("fetching movies...");
-            await fetch("/api/topMovies/25")
+            await fetch(
+                "https://77895cdd-19f0-43f9-b39c-2365a37cbc59.mock.pstmn.io/api/topMovies/25"
+            )
                 .then((response) => response.json())
                 .then((data) => {
                     console.log(data);
                     this.movies = data;
-                }).catch(error => {
-                    console.log('[ERROR]', error);
+                })
+                .catch((error) => {
+                    console.log("[ERROR]", error);
                     return [];
                 });
         },
@@ -65,25 +68,54 @@ export default {
 </script>
 
 <style>
-#app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-    margin-top: 20px;
-    margin-left: 20px;
+
+/* Extra small devices (phones, 600px and down) */
+@media only screen and (max-width: 700px) {
+    #app {
+        /* Large devices (laptops/desktops, 992px and up) */
+        font-family: Avenir, Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 20px;
+        margin-left: 20px;
+    }
+    .grid-container {
+        display: grid;
+        grid-template-columns: auto auto auto auto auto;
+        grid-gap: 10px;
+        padding: 10px;
+    }
+    body {
+        background-color: #3d0000;
+    }
+    img {
+        color: white;
+    }
 }
-.grid-container {
-    display: grid;
-    grid-template-columns: auto auto auto auto auto;
-    grid-gap: 10px;
-    padding: 10px;
-}
-body {
-    background-color: #121314;
-}
-img {
-    color: white;
+@media only screen and (min-width: 700px) {
+    #app {
+        /* Large devices (laptops/desktops, 992px and up) */
+        font-family: Avenir, Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 20px;
+        margin-left: 20px;
+    }
+    .grid-container {
+        display: grid;
+        grid-template-columns: auto auto auto auto auto;
+        grid-gap: 10px;
+        padding: 10px;
+    }
+    body {
+        background-color: #000000;
+    }
+    img {
+        color: white;
+    }
 }
 </style>
--- src/components/HeaderCustom.vue
+++ src/components/HeaderCustom.vue
@@ -1,5 +1,8 @@
 <template>
     <header>
+        <div class="hamburger">
+            <img src="../assets/bars-solid.svg" alt="" srcset="">
+        </div>
         <div class="container-left">
             <a id="logo-text" href="#">SAMFLIX</a>
             <a href="#">home</a>
@@ -24,36 +27,85 @@ export default {
 </script>
 
 <style scoped>
-header {
-    text-align: left;
-    color: whitesmoke;
-    display: block;
-    margin-bottom: 40px;
+/* Extra small devices (phones, 600px and down) */
+@media only screen and (max-width: 700px) {
+    header {
+        text-align: left;
+        color: whitesmoke;
+        display: block;
+        margin-bottom: 40px;
+    }
+    .hamburger {
+        width: 2rem;
+        filter: invert(100%) sepia(100%) saturate(0%) hue-rotate(0deg)
+        brightness(100%) contrast(100%);
+        margin:0%;
+        padding: 0%;
+    }
+    a {
+        display: none;
+        margin-bottom: auto;
+        margin-top: auto;
+        text-decoration: none;
+        margin-right: 20px;
+        text-align: center;
+        font-size: 18px;
+        color: whitesmoke;
+    }
+    #logo-text {
+        margin-bottom: auto;
+        margin-top: auto;
+        margin-left: auto;
+        text-decoration: none;
+        text-align: center;
+        font-size: 36px;
+        color: red;
+        font-weight: bolder;
+    }
+    .container-left {
+        text-align: left;
+        color: whitesmoke;
+        display: inline;
+        float: left;
+        /* padding-right: 0px; */
+    }
 }
-a {
-    margin-bottom: auto;
-    margin-top: auto;
-    text-decoration: none;
-    margin-right: 20px;
-    text-align: center;
-    font-size: 18px;
-    color: whitesmoke;
-}
-#logo-text {
-    margin-bottom: auto;
-    margin-top: auto;
-    margin-left: auto;
-    text-decoration: none;
-    text-align: center;
-    font-size: 36px;
-    color: red;
-    font-weight: bolder;
-}
-.container-left {
-    text-align: left;
-    color: whitesmoke;
-    display: inline;
-    float: left;
-    /* padding-right: 0px; */
+
+@media only screen and (min-width: 700px) {
+    header {
+        text-align: left;
+        color: whitesmoke;
+        display: block;
+        margin-bottom: 40px;
+    }
+    .hamburger {
+        display: none;
+    }
+    a {
+        margin-bottom: auto;
+        margin-top: auto;
+        text-decoration: none;
+        margin-right: 20px;
+        text-align: center;
+        font-size: 18px;
+        color: whitesmoke;
+    }
+    #logo-text {
+        margin-bottom: auto;
+        margin-top: auto;
+        margin-left: auto;
+        text-decoration: none;
+        text-align: center;
+        font-size: 36px;
+        color: red;
+        font-weight: bolder;
+    }
+    .container-left {
+        text-align: left;
+        color: whitesmoke;
+        display: inline;
+        float: left;
+        /* padding-right: 0px; */
+    }
 }
 </style>
